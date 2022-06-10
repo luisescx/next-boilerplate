fe8b984f1953718fc2614fc18c993e6ab80d77e7
luisescx <luis.esculapio@live.com>
2022-06-10 19:28:32 -0300

    feat: Add GlobalStyles and custom _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,25 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
+import GlobalStyles from "styles/global";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Next - Boilerplate</title>
+
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <meta
+          name="description"
+          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+        />
+      </Head>
+
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp;
+export default App;
